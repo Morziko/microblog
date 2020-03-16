dbc1b7b268f79f3b564d55d2dc6a091540eecd5f
Morziko <morziko2@gmail.com>
2020-03-16 18:17:57 +0200

    heroku deployment changes

--- config.py
+++ config.py
@@ -11,31 +11,31 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://***:1****@localhost/***'
+        'postgresql://yurii:11vivozu@localhost/stuff'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
 
-    
+
+
     MAIL_SERVER='smtp.googlemail.com'
     MAIL_PORT=465
     MAIL_USE_SSL=1
-    MAIL_USERNAME = '***'
-    MAIL_PASSWORD= '***'
+    MAIL_USERNAME = 'morozov.ura029@gmail.com'
+    MAIL_PASSWORD= '12vivozu'
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    
-    ADMINS = ['***', '***']
-    
+
+    ADMINS = ['morziko2@gmail.com', 'morozov.yra029@gmail.com']
+
     POSTS_PER_PAGE = 20
-    
+
     LANGUAGES = ['ukr', 'en']
-    
+
     # ELASTICSEARCH_URL = 'http://localhost:9200'
-    
+
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    
+
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 class ProductionConfig(Config):
